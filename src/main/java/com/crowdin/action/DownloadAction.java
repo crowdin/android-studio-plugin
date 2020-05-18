e0fcb1502b361b4800cb0fe37fa19e98186bd5ff
Barabash Daniil <dBarabash42@gmail.com>
2020-05-18 13:48:48 +0300

    In 'download' command filter files that are not related to plugin and auto-reload project at the end

--- src/main/java/com/crowdin/action/DownloadAction.java
+++ src/main/java/com/crowdin/action/DownloadAction.java
@@ -10,8 +10,17 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import net.lingala.zip4j.core.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class DownloadAction extends AnAction {
 
@@ -24,16 +33,39 @@ public class DownloadAction extends AnAction {
         String branch = GitUtil.getCurrentBranch(project);
         File downloadTranslations = crowdin.downloadTranslations(source, branch);
         if (downloadTranslations != null) {
-            this.extractTranslations(downloadTranslations);
+            String tempDir = downloadTranslations.getParent() + File.separator + "all" + System.nanoTime() + File.separator;
+            this.extractTranslations(downloadTranslations, tempDir);
+            List<String> files = walkDir(Paths.get(tempDir)).stream()
+                    .map(File::getAbsolutePath)
+                    .map(path -> StringUtils.removeStart(path, tempDir))
+                    .collect(Collectors.toList());
+
+            List<String> androidCodes = crowdin.getSupportedLanguageAndroidCodes();
+            List<String> sortedFiles = filterFiles(files, androidCodes);
+            sortedFiles.forEach(filePath -> {
+                File fromFile = new File(tempDir + filePath);
+                File toFile = new File(downloadTranslations.getParent() + File.separator + filePath);
+                toFile.getParentFile().mkdirs();
+                if (!fromFile.renameTo(toFile) && toFile.delete() && !fromFile.renameTo(toFile)) {
+                    NotificationUtil.showWarningMessage("Failed to extract file '" + toFile + "'.");
+                }
+            });
             if (downloadTranslations.delete()) {
                 System.out.println("all.zip was deleted");
             } else {
                 System.out.println("all.zip wasn't deleted");
             }
+            try {
+                FileUtils.deleteDirectory(new File(tempDir));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            virtualFile.refresh(true, true);
+            NotificationUtil.showInformationMessage("Translations successfully downloaded");
         }
     }
 
-    private void extractTranslations(File archive) {
+    private void extractTranslations(File archive, String dirPath) {
         if (archive == null) {
             return;
         }
@@ -45,11 +77,31 @@ public class DownloadAction extends AnAction {
         }
 
         try {
-            zipFile.extractAll(archive.getParent());
-            NotificationUtil.showInformationMessage("Translations successfully downloaded");
+            zipFile.extractAll(dirPath);
         } catch (ZipException e) {
             NotificationUtil.showInformationMessage("Downloading translations failed");
             e.printStackTrace();
         }
+    }
+
+    private List<File> walkDir(Path dir) {
+        try {
+            return java.nio.file.Files.walk(dir)
+                .filter(java.nio.file.Files::isRegularFile)
+                .map(Path::toFile)
+                .collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private List<String> filterFiles(List<String> files, List<String> androidCodes) {
+        Pattern pattern = Pattern.compile("[\\\\\\\\\\/]?values-([a-zA-Z-]+)[\\\\\\\\\\/]([^\\\\\\\\|^\\/]+)$");
+        return files.stream()
+            .filter(file -> {
+                Matcher matcher = pattern.matcher(file);
+                return (matcher.matches() && androidCodes.contains(matcher.group(1)));
+            })
+            .collect(Collectors.toList());
     }
 }
