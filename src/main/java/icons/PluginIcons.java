af0ace26d056c7492acb3704f3d727bae38cf6c5
Yevheniy Oliynyk <evgen41293@gmail.com>
2020-05-20 18:41:30 +0300

    feat: removed not needed icons

--- src/main/java/icons/PluginIcons.java
+++ src/main/java/icons/PluginIcons.java
@@ -5,8 +5,5 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public interface PluginIcons {
-
     Icon ICON = IconLoader.getIcon("/icons/icon.svg");
-    Icon UPLOAD = IconLoader.getIcon("/icons/upload.svg");
-    Icon DOWNLOAD = IconLoader.getIcon("/icons/download.svg");
 }
