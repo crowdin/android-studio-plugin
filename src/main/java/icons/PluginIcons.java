d78b894c0e871d3744c7d5d16b3729dcec864acf
Yevheniy Oliynyk <evgen41293@gmail.com>
2020-05-20 16:27:16 +0300

    feat: added icons

---
+++ src/main/java/icons/PluginIcons.java
@@ -0,0 +1,12 @@
+package icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public interface PluginIcons {
+
+    Icon ICON = IconLoader.getIcon("/icons/icon.svg");
+    Icon UPLOAD = IconLoader.getIcon("/icons/upload.svg");
+    Icon DOWNLOAD = IconLoader.getIcon("/icons/download.svg");
+}
