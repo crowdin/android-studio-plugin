96638ba1bb6a6dc2f2cd144a3ea50120014df393
Yevheniy Oliynyk <evgen41293@gmail.com>
2020-05-21 19:37:45 +0300

    fix: fixed work in multiple projects & removed error notification if project does not have crowdin property file

--- src/main/java/com/crowdin/activity/CrowdinStartupActivity.java
+++ src/main/java/com/crowdin/activity/CrowdinStartupActivity.java
@@ -2,6 +2,7 @@ package com.crowdin.activity;
 
 import com.crowdin.client.Crowdin;
 import com.crowdin.event.FileChangeListener;
+import com.crowdin.util.PropertyUtil;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
 import org.jetbrains.annotations.NotNull;
@@ -10,8 +11,12 @@ public class CrowdinStartupActivity implements StartupActivity {
 
     @Override
     public void runActivity(@NotNull Project project) {
+        System.out.println("On");
         new FileChangeListener(project);
-        //config validation
-        new Crowdin(project);
+        System.out.println(project);
+        if (PropertyUtil.getCrowdinPropertyFile(project) != null) {
+            //config validation
+            new Crowdin(project);
+        }
     }
 }
