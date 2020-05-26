a9f91c1b31d5abd83f0d58acfec84de519d65cb0
Barabash Daniil <dBarabash42@gmail.com>
2020-05-26 15:32:43 +0300

    Handle exceptions for 'upload translations' and swap commands

--- src/main/java/com/crowdin/action/UploadTranslationsAction.java
+++ src/main/java/com/crowdin/action/UploadTranslationsAction.java
@@ -27,49 +27,49 @@ public class UploadTranslationsAction extends BackgroundAction {
         CrowdinProperties properties;
         try {
             properties = CrowdinPropertiesLoader.load(project);
+            Crowdin crowdin = new Crowdin(project, properties.getProjectId(), properties.getApiToken(), properties.getBaseUrl());
+
+            List<Language> projectLanguages = crowdin.getProjectLanguages();
+
+            String branch = properties.isDisabledBranches() ? "" : GitUtil.getCurrentBranch(project);
+            Long branchId = crowdin.getBranch(branch).map(Branch::getId).orElse(null);
+
+            List<com.crowdin.client.sourcefiles.model.File> files = crowdin.getFiles(branchId);
+            Map<Long, Directory> dirs = crowdin.getDirectories(branchId);
+            Map<String, File> filePaths = CrowdinFileUtil.buildFilePaths(files, dirs);
+
+            AtomicInteger uploadedFilesCounter = new AtomicInteger(0);
+
+            properties.getSourcesWithPatterns().forEach((sourcePattern, translationPattern) -> {
+                List<VirtualFile> sources = FileUtil.getSourceFilesRec(root, sourcePattern);
+                sources.forEach(source -> {
+                    VirtualFile baseDir = FileUtil.getBaseDir(source, sourcePattern);
+                    String sourcePath = source.getName();
+
+                    File crowdinSource = filePaths.get(sourcePath);
+                    if (crowdinSource == null) {
+                        NotificationUtil.showWarningMessage(project, "File '" + (branch != null ? branch + "/" : "") + sourcePath + "' is missing in the project. Run 'Upload' to upload the missing source");
+                        return;
+                    }
+                    String pattern1 = PlaceholderUtil.replaceFilePlaceholders(translationPattern, sourcePath);
+                    for (Language lang : projectLanguages) {
+                        String pattern2 = PlaceholderUtil.replaceLanguagePlaceholders(pattern1, lang);
+                        java.io.File translationFile = new java.io.File(baseDir.getPath() + "/" + pattern2);
+                        if (!translationFile.exists()) {
+                            continue;
+                        }
+                        boolean uploaded = crowdin.uploadTranslationFile(translationFile, crowdinSource.getId(), lang.getId());
+                        if (uploaded) {
+                            uploadedFilesCounter.incrementAndGet();
+                        }
+                    }
+                });
+            });
+            NotificationUtil.showInformationMessage(project, "Uploaded " + uploadedFilesCounter.get() + " files");
         } catch (Exception exception) {
             NotificationUtil.showErrorMessage(project, exception.getMessage());
             return;
         }
-        Crowdin crowdin = new Crowdin(project, properties.getProjectId(), properties.getApiToken(), properties.getBaseUrl());
-
-        List<Language> projectLanguages = crowdin.getProjectLanguages();
-
-        String branch = properties.isDisabledBranches() ? "" : GitUtil.getCurrentBranch(project);
-        Long branchId = crowdin.getBranch(branch).map(Branch::getId).orElse(null);
-
-        List<com.crowdin.client.sourcefiles.model.File> files = crowdin.getFiles(branchId);
-        Map<Long, Directory> dirs = crowdin.getDirectories(branchId);
-        Map<String, File> filePaths = CrowdinFileUtil.buildFilePaths(files, dirs);
-
-        AtomicInteger uploadedFilesCounter = new AtomicInteger(0);
-
-        properties.getSourcesWithPatterns().forEach((sourcePattern, translationPattern) -> {
-            List<VirtualFile> sources = FileUtil.getSourceFilesRec(root, sourcePattern);
-            sources.forEach(source -> {
-                VirtualFile baseDir = FileUtil.getBaseDir(source, sourcePattern);
-                String sourcePath = source.getName();
-
-                File crowdinSource = filePaths.get(sourcePath);
-                if (crowdinSource == null) {
-                    NotificationUtil.showWarningMessage(project, "File '" + (branch != null ? branch + "/" : "") + sourcePath + "' is missing in the project. Run 'Upload' to upload the missing source");
-                    return;
-                }
-                String pattern1 = PlaceholderUtil.replaceFilePlaceholders(translationPattern, sourcePath);
-                for (Language lang : projectLanguages) {
-                    String pattern2 = PlaceholderUtil.replaceLanguagePlaceholders(pattern1, lang);
-                    java.io.File translationFile = new java.io.File(baseDir.getPath() + "/" + pattern2);
-                    if (!translationFile.exists()) {
-                        continue;
-                    }
-                    boolean uploaded = crowdin.uploadTranslationFile(translationFile, crowdinSource.getId(), lang.getId());
-                    if (uploaded) {
-                        uploadedFilesCounter.incrementAndGet();
-                    }
-                }
-            });
-        });
-        NotificationUtil.showInformationMessage(project, "Uploaded " + uploadedFilesCounter.get() + " files");
     }
 
     @Override
