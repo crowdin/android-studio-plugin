4a806bfdaceab339e8d160da800670d80e787bd3
sivasuriyankumarasamy <sivasuriyankumarasamy@gmail.com>
2022-10-08 21:54:04 +0530

    Added Test cases for PlaceholderUtil.class

--- src/test/java/com/crowdin/util/PlaceholderUtilTest.java
+++ src/test/java/com/crowdin/util/PlaceholderUtilTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.HashMap;
+import java.util.*;
 import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
@@ -40,5 +40,27 @@ public class PlaceholderUtilTest {
         return Stream.of(
             arguments(STANDARD_PATTERN, LanguageBuilder.UKR.build(), "/values-uk-rUA/%original_file_name%")
         );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    public void testBuildTranslationPatterns(String translationPattern, List<Language> projLanguages, LanguageMapping languageMapping, String relativeSourcePath, Map<Language, String> expected) {
+        Map<Language, String> actual = PlaceholderUtil.buildTranslationPatterns(relativeSourcePath, translationPattern, projLanguages, languageMapping);
+        assertEquals(String.format("pattern: %s, actual: %s, expected: %s", translationPattern, actual, expected), expected, actual);
+    }
+
+    public static Stream<Arguments> testBuildTranslationPatterns() {
+        return Stream.of(arguments(STANDARD_PATTERN, Arrays.asList(LanguageBuilder.UKR.build()), LanguageMapping.fromServerLanguageMapping(new HashMap<>()), "values/strings.xml", Collections.singletonMap(LanguageBuilder.UKR.build(), "/values-uk-rUA/strings.xml")));
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    public void testFormatSourcePatternForRegex(String toFormat, String expected) {
+         String actual = PlaceholderUtil.formatSourcePatternForRegex(toFormat);
+        assertEquals(String.format("pattern: %s, actual: %s, expected: %s", toFormat, actual, expected), expected, actual);
+    }
+
+    public static Stream<Arguments> testFormatSourcePatternForRegex() {
+        return Stream.of(arguments(STANDARD_PATTERN, "/values-%android_code%/[^/]+"));
     }
 }
