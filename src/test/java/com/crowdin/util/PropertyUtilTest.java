45c2bb835dc2259e40f10873c125e77254cbcebb
Gowrishankar04 <shankargowri906@gmail.com>
2022-10-12 23:44:46 +0530

    Added Unit Test Case For PropertyUtil

--- src/test/java/com/crowdin/util/PropertyUtilTest.java
+++ src/test/java/com/crowdin/util/PropertyUtilTest.java
@@ -1,76 +1,45 @@
 package com.crowdin.util;
-
-import com.crowdin.client.CrowdinProperties;
-import com.crowdin.client.CrowdinPropertiesLoader;
-import com.crowdin.client.FileBean;
-import com.crowdin.client.FileBeanBuilder;
+import com.intellij.testFramework.fixtures.BasePlatformTestCase;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
 import java.util.stream.Stream;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
-public class PropertyUtilTest {
+public class PropertyUtilTest extends BasePlatformTestCase {
+
+    @Override
+    protected String getTestDataPath() {
+        return "src/test/testData";
+    }
+
+    @BeforeEach
+    public void setup() throws Exception {
+        super.setUp();
+    }
+
+    @AfterEach
+    public void teardown() throws Exception {
+        super.tearDown();
+    }
 
     @ParameterizedTest
     @MethodSource
-    public void testGetSources(Properties properties, List<FileBean> expected) {
-        CrowdinProperties result = CrowdinPropertiesLoader.load(properties);
-        assertEquals("Properties: " + properties, expected, result.getFiles());
+    public void testGetSources(String project, String expected, String key) {
+        myFixture.copyFileToProject(project);
+        String result = PropertyUtil.getPropertyValue(key, myFixture.getProject());
+        assertEquals(expected, result);
     }
 
     public static Stream<Arguments> testGetSources() {
-        ClassLoader classLoader = PropertyUtilTest.class.getClassLoader();
         return Stream.of(
-            arguments(
-                getProperties(new java.io.File(classLoader.getResource("properties/sources/one_source.properties").getFile())),
-                new ArrayList<FileBean>() {{
-                    add(FileBeanBuilder.fileBean("**/values/strings.xml", "/values-%android_code%/%original_file_name%").build());
-                }}),
-            arguments(
-                    getProperties(new java.io.File(classLoader.getResource("properties/sources/list_sources.properties").getFile())),
-                new ArrayList<FileBean>() {{
-                    add(FileBeanBuilder.fileBean("**/values/strings.xml", "/values-%android_code%/%original_file_name%").build());
-                    add(FileBeanBuilder.fileBean("**/values/strings1.xml", "/values-%android_code%/%original_file_name%").build());
-                    add(FileBeanBuilder.fileBean("**/values/strings2.xml", "/values-%android_code%/%original_file_name%").build());
-                }}),
-            arguments(
-                    getProperties(new java.io.File(classLoader.getResource("properties/sources/sources_w_translations.properties").getFile())),
-                new ArrayList<FileBean>() {{
-                    add(FileBeanBuilder.fileBean("values/*.xml", "/values-%android_code%/%original_file_name%").build());
-                    add(FileBeanBuilder.fileBean("another/path/*.xml", "/another/path-%android_code%/%original_file_name%").build());
-                    add(FileBeanBuilder.fileBean("values2/*.xml", "/values2-%android_code%/%original_file_name%").build());
-                }}),
-            arguments(
-                    getProperties(new java.io.File(classLoader.getResource("properties/sources/combined_sources.properties").getFile())),
-                new ArrayList<FileBean>() {{
-                    add(FileBeanBuilder.fileBean("**/values/strings.xml", "/values-%android_code%/%original_file_name%").build());
-                    add(FileBeanBuilder.fileBean("**/values/strings1.xml", "/values-%android_code%/%original_file_name%").build());
-                    add(FileBeanBuilder.fileBean("**/values/strings2.xml", "/values-%android_code%/%original_file_name%").build());
-                    add(FileBeanBuilder.fileBean("values/*.xml", "/values-%android_code%/%original_file_name%").build());
-                    add(FileBeanBuilder.fileBean("another/path/*.xml", "/another/path-%android_code%/%original_file_name%").build());
-                    add(FileBeanBuilder.fileBean("values2/*.xml", "/values2-%android_code%/%original_file_name%").build());
-                }})
+            arguments("values/strings2.xml",
+                    "", null)
         );
     }
 
-    public static Properties getProperties(java.io.File file) {
-        Properties properties = new Properties();
-        try {
-            InputStream in = new FileInputStream(file.getCanonicalPath());
-            properties.load(in);
-            in.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return properties;
-    }
 }
