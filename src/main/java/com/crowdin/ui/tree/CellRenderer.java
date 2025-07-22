7407a46e8df212ec19e80f78fa1ddbb593258a59
Max Inno <40697586+innomaxx@users.noreply.github.com>
2025-07-22 14:46:36 +0300

    fix: IDE error: Cannot read field "x" because "nd" is null (#149)

--- src/main/java/com/crowdin/ui/tree/CellRenderer.java
+++ src/main/java/com/crowdin/ui/tree/CellRenderer.java
@@ -12,10 +12,10 @@ public class CellRenderer extends DefaultTreeCellRenderer {
 
     @Override
     public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel, boolean expanded, boolean leaf, int row, boolean hasFocus) {
-        super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf, row, hasFocus);
+        var parentComponent = super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf, row, hasFocus);
         CellData cellData = CellRenderer.getData(value);
         if (cellData == null) {
-            return null;
+            return parentComponent;
         }
 
         JBLabel label = new JBLabel(cellData.getText());
